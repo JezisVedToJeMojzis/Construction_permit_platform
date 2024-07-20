65a5701ce3a8fc6a81b6fadf4711bc38503efff5
JezisVedToJeMojzis <mojzissamuel@gmail.com>
2024-07-20 23:53:01 +0200

    enabled view objections button

--- frontend/dashboard/admin_dashboard.php
+++ frontend/dashboard/admin_dashboard.php
@@ -14,7 +14,7 @@ session_start();
 <div class="menu">
     <a href="admin_dashboard.php">View Dashboard</a>
     <a href="../application/viewApplications.html">View Applications</a>
-    <a href="viewObjections.php">View Objections</a>
+    <a href="../objection/viewObjections.html">View Objections</a>
     <a href="../../backend/authentication/logout.php">Log Out</a>
 </div>
 <div class="content">
--- frontend/dashboard/user_dashboard.php
+++ frontend/dashboard/user_dashboard.php
@@ -17,7 +17,7 @@ session_start();
     <a href="../profile/viewProfile.php">View Profile</a>
     <a href="../application/submitApplication.html">Submit Application</a>
     <a href="../application/viewApplications.html">View Applications</a>
-    <a href="viewObjections.php">View Objections</a>
+    <a href="../objection/viewObjections.html">View Objections</a>
     <a href="../../backend/authentication/logout.php">Log Out</a>
 </div>
 
